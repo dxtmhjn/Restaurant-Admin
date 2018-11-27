2777486d35b3937e0bc0b4a4d6c49d8ea442559e
shubham <shubham.pandey128@gmail.com>
2018-11-27 07:28:10 +0530

    userlist

--- src/Components/Admin/UserList.js
+++ src/Components/Admin/UserList.js
@@ -1,35 +1,39 @@
 import React from "react";
 
 
-const userList = (props) => {
-   
+const singleUser =(
+    <table className="table table-bordered table-striped">
+    <tr>
+        <th>User Id</th>
+        <th>Details</th>
+        <th>Access</th>
+    </tr>
+    <tbody>
+        <tr>
+            <td>1232344</td>
+            <td>yogesh,Restaurant admin</td>
+            <td>
+            <span><a href="#" className="btn btn-success">Edit</a></span> &nbsp;
+            <span><a href="#" className="btn btn-danger">Delete</a></span>
+            </td>
+        </tr>
+    </tbody>
+</table>
+)
+const userList = () =>  {
     return(
        <div className="container">
        <div className="row">
        <div className="col-sm-12">
        <h2>All Registered List</h2>
        <br/>
-       <table className="table table-bordered table-striped">
-            <tr>
-                <th>User Id</th>
-                <th>Details</th>
-                <th>Access</th>
-            </tr>
-            <tbody>
-                <tr>
-                    <td>1234</td>
-                    <td>yogesh,Restaurant admin</td>
-                    <td>
-                    <span><a href="#" className="btn btn-success">Edit</a></span> &nbsp;
-                    <span><a href="#" className="btn btn-danger">Delete</a></span>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+            {singleUser}
        </div>
        </div>
        </div>
     )
-}
+
+    }
+
 
 export default userList;
